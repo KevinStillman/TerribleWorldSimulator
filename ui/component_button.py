688142c4784ab468c08c188aaee79d0d12463c6d
ChaeDLR <delarosa.chae@gmail.com>
2021-03-31 12:44:02 -0400

    Cleaning up component_button. basic_ui, and did some reformatting.
    Component button now inherits from component_base. button set_position method calls parent method set_coors and uses the variable to set the button and text position. Added method to button that will set the button label to the default string passed in the buttons constructor or will take an optional argument of type string and will set the text to that. _load_buttons method in basic_ui now requires each buttons set_position method to be called instead of set_coors. I moved all of the ui blits into their own methods and collected them into a method called _blit_ui_components. Now update only needs to call the one method and we can easily choose what ui elements appear above others in the _blit_ui_components method.

--- ui/component_button.py
+++ ui/component_button.py
@@ -3,12 +3,14 @@ from src.colors import Firebrick, Black
 from ui.component_base import Component_Base
 
 
-class Component_Button(pygame.Surface):
-    ''' play button to start the sim '''
+class Component_Button(Component_Base):
+    """ play button to start the sim """
 
     def __init__(self, label: str, button_width: int, button_height: int):
-        ''' tws arg is 'self' from the game_loop object '''
-        super().__init__((button_width, button_height))
+        """
+        Creating a button requires a label and the buttons dims
+        """
+        super().__init__(button_width, button_height)
 
         # just place holders
         self.x_coors, self.y_coors = 0, 0
@@ -16,27 +18,38 @@ class Component_Button(pygame.Surface):
         self.text_color = Black
         self.font = pygame.font.SysFont(None, 48, bold=True)
         self.button_label = label
-        
+
         self._build_button(button_width, button_height)
 
-    def set_coors(self, x: int, y: int):
-        ''' set buttons x and y position '''
-        self.x_coors, self.y_coors = x, y
-        self.rect.x = x
-        self.rect.y = y
-
+    def set_position(self, x: int, y: int):
+        """ set buttons x and y position """
+        # call the parent method to set the coors then add code to set text position
+        self.set_coors(x, y)
+        # set position from coors which is stored in the parent class
+        self.rect.x = self.coors[0]
+        self.rect.y = self.coors[1]
         self.text_rect.center = self.rect.center
-
-    def _build_button(self, width: int, height: int):
-        ''' create the button and set its position '''
-        self.rect = pygame.Rect(0, 0, width, height)
-
-        self.text = self.font.render(
-            self.button_label, True, self.text_color, self.button_color
+    
+    def set_new_label(self, label: str=None):
+        """ Set a new label for the button """
+        # if there is no label passed
+        # use the label string we got from the constructor
+        if not label:
+            self.text = self.font.render(
+                self.button_label, True, self.text_color, self.button_color
+            )
+        else:
+            self.text = self.font.render(
+                label, True, self.text_color, self.button_color
             )
 
         self.text_rect = self.text.get_rect()
         self.text_rect.center = self.rect.center
 
+
+    def _build_button(self, width: int, height: int):
+        """ create the button and set its position """
+        self.rect = pygame.Rect(0, 0, width, height)
+        self.set_new_label()
         self.fill(self.button_color)
         self.blit(self.text, self.text_rect)
